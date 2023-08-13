07866cb8f878b13147c3ccb4e914f810f2a9c80e
junchaoma423 <87057641+junchaoma423@users.noreply.github.com>
2023-08-13 12:27:46 -0700

    Update FSMState_MPCStand.cpp

--- src/FSM/FSMState_MPCStand.cpp
+++ src/FSM/FSMState_MPCStand.cpp
@@ -28,14 +28,14 @@ void FSMState_MPCStand::run()
 
     std::cout << "Current state is MPC " << std::endl;
 
-    pd = get_pose_data(pipe);
+    //pd = get_pose_data(pipe);
 
-    T265_pose[0] = pd.x;
-    T265_pose[1] = pd.y;
-    T265_pose[2] = pd.z;
-    T265_pose[3] = pd.x_vel;
-    T265_pose[4] = pd.y_vel;
-    T265_pose[5] = pd.z_vel;
+    //T265_pose[0] = pd.x;
+    //T265_pose[1] = pd.y;
+    //T265_pose[2] = pd.z;
+    //T265_pose[3] = pd.x_vel;
+    //T265_pose[4] = pd.y_vel;
+    //T265_pose[5] = pd.z_vel;
     
     T265_pos << -T265_pose[2] << "  " << -T265_pose[0] << " " << T265_pose[1] << "  " << -T265_pose[5] << " " << -T265_pose[3] << " " << T265_pose[4];
 
